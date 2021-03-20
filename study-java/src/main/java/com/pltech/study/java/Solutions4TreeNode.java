47ee90a8d0ff162c4db70a221b933d3f2a01fe4e
pangli <pangli@58.com>
2021-03-20 20:40:22 +0800

    完善二叉树的几种遍历算法

--- study-java/src/main/java/com/pltech/study/java/Solutions4TreeNode.java
+++ study-java/src/main/java/com/pltech/study/java/Solutions4TreeNode.java
@@ -10,13 +10,19 @@ import java.util.Stack;
  */
 public class Solutions4TreeNode {
 
+    /**
+     * 二叉树是指树中节点的度不大于2的有序树，所以二叉树的节点中存有值（val）、左子节点（left）和右子节点（right）。
+     * 与二叉树关联的面试问题一般都重点考察二叉树的各种遍历，二叉树的遍历方式主要有：前序遍历、中序遍历和后续遍历，
+     * 而这些遍历方式都可以有递归实现和迭代实现。
+     * 遍历的时间复杂度均为O(n)，每个结点都只遍历一次，并且每个结点访问只需要O(1)复杂度的时间；
+     * 空间复杂度为O(h)，其中h为树的高度。有一种Morris遍历的算法，其空间复杂度为O(1)。
+     */
     private static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
 
-        TreeNode() {
-        }
+        TreeNode() {}
 
         TreeNode(int val) {
             this.val = val;
@@ -29,13 +35,16 @@ public class Solutions4TreeNode {
         }
     }
 
+
+    /* *****************************前序遍历************************************** */
+
     /**
      * 递归方法实现先序遍历二叉树
      *
      * @param root 树根节点
      * @return 节点值的先序序列
      */
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> preOrderTraversal1(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
@@ -58,7 +67,7 @@ public class Solutions4TreeNode {
      * @param root 树根节点
      * @return 节点值的先序序列
      */
-    public List<Integer> preOrderTraversal(TreeNode root) {
+    public List<Integer> preOrderTraversal2(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
@@ -76,7 +85,27 @@ public class Solutions4TreeNode {
 
         return result;
     }
+    /**
+     * 迭代方法实现先序遍历二叉树
+     */
+    public List<Integer> preOrderTraversal3(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                result.add(root.val);
+                root = root.left;
+            }
+            // 当无法压栈的时候，将root.right进行压栈
+            root = stack.pop();
+            root = root.right;
+        }
+        return result;
+    }
 
+
+    /* *****************************中序遍历************************************** */
 
     /**
      * 递归实现中序遍历二叉树
@@ -120,12 +149,109 @@ public class Solutions4TreeNode {
         return result;
     }
 
+
+    /* *****************************后序遍历************************************** */
+
+    /**
+     * 递归实现后续遍历二叉树
+     *
+     * @param root 二叉树根节点
+     * @return 节点值的后序序列
+     */
+    public List<Integer> postOrderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postOrder(root, result);
+        return result;
+    }
+    private void postOrder(TreeNode root, List<Integer> ans) {
+        if (root != null) {
+            // 先遍历左子树
+            postOrder(root.left, ans);
+            // 最后遍历右子树
+            postOrder(root.right, ans);
+            // 然后遍历中间的根结点
+            ans.add(root.val);
+        }
+    }
+
+    /**
+     * 迭代方式实现后序遍历二叉树
+     *
+     * @param root 二叉树根节点
+     * @return 节点值的后续序列
+     */
+    public List<Integer> postOrderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        TreeNode preNode = null; //记录前一个被遍历的节点
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            // 顺着左子树走，将所有左子节点入栈
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            // 当没有左子节点时，考察栈顶节点，注意此时不要出栈
+            // 因为此时的栈顶节点可能有最子树没有遍历
+            root = stack.peek();
+            // 如果要遍历当前结点，需要确保右子树已经遍历完毕
+            // 1. 如果当前结点右子树为空，那么右子树没有遍历的必要，将当前结点放到result中
+            // 2. 当root.right == preNode时，说明右子树已经被打印过了，此时要将当前结点放到result中
+            if (root.right == null || root.right == preNode) {
+                // 栈顶节点的右子树已遍历完毕，将此节点放入结果列表
+                result.add(root.val);
+                stack.pop();    // 将栈顶节点出栈
+                preNode = root; // 更新前一个遍历的节点
+                root = null;    //已经打印完毕。需要设置为空，否则下一轮循环还会遍历到其左子树
+            } else {
+                // 第一次走到root结点，不能放到result中，因为root的右子树还没有遍历,
+                // 需要将root结点的右子树遍历
+                root = root.right;
+            }
+        }
+        return result;
+    }
+
+
+    /* *****************************Morris遍历*********************************** */
+
+    /**
+     * Morris 前序遍历
+     *
+     * @param root 二叉树根节点
+     * @return 前序序列
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        TreeNode cur = root;
+        List<Integer> ans = new ArrayList<>();
+
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode pre = cur.left;
+                while (pre.right != null && pre.right != cur) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    ans.add(cur.val);
+                    pre.right = cur;
+                    cur = cur.left;
+                } else {
+                    pre.right = null;
+                    cur = cur.right;
+                }
+            } else {
+                ans.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return ans;
+    }
+
     /**
      * Morris 中序遍历
      * 如果 xx 无左孩子，先将 xx 的值加入答案数组，再访问 xx 的右孩子，即 x = x.right。
      * 如果 xx 有左孩子，则找到 xx 左子树上最右的节点（即左子树中序遍历的最后一个节点，xx 在中序遍历中的前驱节点），我们记为predecessor。根据predecessor 的右孩子是否为空，进行如下操作。
-     * 	如果 predecessor 的右孩子为空，则将其右孩子指向 xx，然后访问 xx 的左孩子，即 x = x.left。
-     * 	如果 predecessor 的右孩子不为空，则此时其右孩子指向 xx，说明我们已经遍历完 xx 的左子树，我们将 predecessor 的右孩子置空，将 xx 的值加入答案数组，然后访问 xx 的右孩子，即 x = x.right。
+     * 如果 predecessor 的右孩子为空，则将其右孩子指向 xx，然后访问 xx 的左孩子，即 x = x.left。
+     * 如果 predecessor 的右孩子不为空，则此时其右孩子指向 xx，说明我们已经遍历完 xx 的左子树，我们将 predecessor 的右孩子置空，将 xx 的值加入答案数组，然后访问 xx 的右孩子，即 x = x.right。
      * 重复上述操作，直至访问完整棵树
      *
      * @param root 树根节点
