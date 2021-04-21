a3f4302f629137ab4940329fe17b2dea51a23c1d
pangli <pangli@58.com>
2021-04-21 09:49:06 +0800

    添加用堆求解数组中k个最小数的问题

--- study-java/src/main/java/com/pltech/study/java/Solutions4Array.java
+++ study-java/src/main/java/com/pltech/study/java/Solutions4Array.java
@@ -1,5 +1,6 @@
 package com.pltech.study.java;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -7,6 +8,16 @@ import java.util.HashMap;
  * Created by Pang Li on 2021/2/27
  */
 public class Solutions4Array {
+
+    public static void main(String[] args) {
+        int[] heights = {0,2, 1, 5, 6, 4, 3, 7};
+        //int[] heights = {1, 1};
+        Solutions4Array solution = new Solutions4Array();
+        int[] res = solution.smallestK_(heights, 4);
+        System.out.println(Arrays.toString(res));
+    }
+
+    //*******************************************************************************************//
 
     /**
      * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
@@ -30,6 +41,8 @@ public class Solutions4Array {
         }
     }
 
+
+    //*******************************************************************************************//
 
     /**
      * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
@@ -61,6 +74,14 @@ public class Solutions4Array {
         throw new IllegalArgumentException("there are no two sum numbers.");
     }
 
+
+    //*******************************************************************************************//
+
+    /**
+     * 去除数组中重复的数字
+     *
+     * @param nums 原数组
+     */
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -76,4 +97,121 @@ public class Solutions4Array {
         }
         return tempIndex + 1;
     }
+
+
+    //*******************************************************************************************//
+
+    /**
+     * https://leetcode-cn.com/problems/smallest-k-lcci/
+     * 设计一个算法，找出数组中最小的k个数。以任意顺序返回这k个数均可。
+     * <p>
+     * 本解法是采用构建k个数的大顶堆，然后动态更新大顶堆，遍历完数组后大顶堆中的元素就是数组中最小的k个数了
+     *
+     * @param arr 原数组
+     * @param k   k
+     * @return 原数组中k个最小的数
+     */
+    public int[] smallestK(int[] arr, int k) {
+        int[] ans = new int[k];
+        if (arr == null || k < 1) return ans;
+        int length = arr.length;
+        if (length <= k) return arr;
+
+        buildMaxRootHeap(arr, k);
+        for (int i = k; i < arr.length; i++) {
+            // 因为是用的大顶堆，这里的条件应为待比较的元素比堆顶元素小的时候，才替换堆顶元素并调整堆结构
+            if (arr[i] < arr[0]) {
+                arr[0] = arr[i];
+                adjustHeap(arr, 0, k);
+            }
+        }
+        System.arraycopy(arr, 0, ans, 0, k);
+        return ans;
+    }
+    // 构建大顶堆
+    private void buildMaxRootHeap(int[] array, int length) {
+        for (int i = (length-2) >> 1; i >= 0; i--) {
+            adjustHeap(array, i, length);
+        }
+    }
+    // 调整堆顶元素，维持堆结构为大顶堆
+    private void adjustHeap(int[] arr, int parent, int len) {
+        int temp = arr[parent];
+        int child = 2 * parent + 1;
+        while (child < len) {
+            if (child + 1 < len && arr[child] <= arr[child+1]) {
+                child++;
+            }
+            if (temp >= arr[child]) {
+                break;
+            }
+            arr[parent] = arr[child];
+            parent = child;
+            child = 2 * parent + 1;
+        }
+        arr[parent] = temp;
+    }
+
+
+    /**
+     * https://leetcode-cn.com/problems/smallest-k-lcci/
+     * 设计一个算法，找出数组中最小的k个数。以任意顺序返回这k个数均可。
+     * <p>
+     * 本解法采用小顶堆
+     *
+     * @param arr 原数组
+     * @param k   k
+     * @return 原数组中k个最小的数
+     */
+    public int[] smallestK_(int[] arr, int k) {
+        int len = arr.length;
+        if (k >= len) {
+            return arr;
+        }
+
+        buildMinHeap(arr, len);
+
+        int pos = len - k;
+        for (int i = len - 1; i >= pos; i--) {
+            swap(arr, 0, i);
+            heapify(arr, 0, --len);
+        }
+
+        int[] ret = new int[k];
+        System.arraycopy(arr, pos, ret, 0, k);
+        return ret;
+    }
+    private void buildMinHeap(int[] arr, int len) {
+        for (int i = (len - 1) / 2; i >= 0; i--) {
+            heapify(arr, i, len);
+        }
+    }
+    private void heapify(int[] arr, int i, int len) {
+        if (i >= len) return;
+
+        int min = i;
+        int c1 = 2 * i + 1;
+        int c2 = 2 * i + 2;
+
+        if (c1 < len && arr[c1] < arr[min]) {
+            min = c1;
+        }
+        if (c2 < len && arr[c2] < arr[min]) {
+            min = c2;
+        }
+
+        if (min != i) {
+            swap(arr, i, min);
+            heapify(arr, min, len);
+        }
+    }
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
+    //*******************************************************************************************//
+
 }
